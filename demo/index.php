b8b5c484109ace9920bbf36d324e72680aab2cfd
Viswas <normalestusername@gmail.com>
2020-07-01 19:16:04 +0400

    Initial commit

---
+++ demo/index.php
@@ -0,0 +1,252 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Navarasangal.com</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- master css -->
+    <link href="css/master.css" rel="stylesheet">
+    <link href="css/color7.css" rel="stylesheet">
+
+    <link href="css/hover.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <script src="js/modernizr.custom.js"></script>
+    <link rel="shortcut icon" href="images/favicon.ico">
+    <link href='https://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800,900' rel='stylesheet'
+        type='text/css'>
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+    <!--==========================================slider section=============================================-->
+    <div class="banner3 slider-version7 maindv-element" style="display:none !important;" id="topp">
+        <div class="container">
+            <div class="row  padding-top-30">
+
+                <div class="col-md-12 col-sm-12 col-xs-12 banner3-content padding-top-00" id="content-version7">
+
+                    <div class="other-content text-center ">
+                        <img src="navarasangal.gif" width="30%" />
+
+                    </div>
+
+                </div>
+
+                <div style="display:none;" class="top-address-bar" id="topbar-version7">
+                    <div class="Center">
+                        <img src="logo.gif"></div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+    <!--==========================================TOP NAV=============================================-->
+
+
+
+    <div class="service3-section padding-top-00 maindv-content1" style="display:none !important;" id="1">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-sm-8 col-xs-12 service3-main" id="service-version7">
+
+                    <p class="text-white text-justify padding-top-20" style="padding-left: 15%;padding-right: 15%;text-align:justify;">
+					Navarsam is a significant concept in the Indian art and cultural history. It symbolizes the varied human emotions - anger, disgust, fear, sadness, wonder, joy, love, courage and peace. Aren't these the same emotions that we go through on a daily basis? Navarsangal.com is a humble attempt to trigger these varied emotions in you and share a platform to recognize the amazing ways in which you can portray those with the right level of originality and quality for entertainment. We are coming with more fun things to do here soon. Meanwhile scroll down to see interesting content posted online..
+
+
+                    </p>
+                    <div class="arrow bounce maindv-content2">
+                        <a class="fa fa-chevron-down fa-2x" href="#" style="color: #555;"></a>
+                    </div>
+
+                    <div class="clearfix"></div>
+
+                </div>
+
+            </div>
+            <div class="row padding-top-20">
+                <div class="content-head center">
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="service3-section padding-top-35 maindv-content2"
+        style="background-color: #cccccc;display:none !important;" id="1">
+        <div class="container">
+            <div class="row" id="tagrowdiv">
+
+                <div class="taggbox-container"
+                    style=" width:100%;height:100%;overflow: auto;background-color: #cccccc;">
+                    <script defer src="//web.taggbox.com/app/js/embed.min.js" type="text/javascript"></script>
+                    <div class="taggbox-socialwall" data-wall-id="33674" view-url="https://taggbox.com/w/33674"> </div>
+                </div>
+
+            </div>
+            <div class="row padding-top-20">
+                <div class="content-head center">
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <!--==========================================subscribe form section=============================================-->
+
+    <div class="clearfix"></div>
+
+    <div class="top-social3 padding-top-10 padding-bottom-10 maindv-content2" style="display:none;" id="nav-version7">
+
+        <div class="container top-nav clearfix maindv-content2-responsive ">
+            <div class="row">
+                
+                <div class="col-md-8 col-sm-8 col-xs-12  ">
+                    <div class="navbar5-logo col-md-12 col-sm-12 col-xs-12 ">
+                        <a href="index.php" class=""><img alt="" style="width:15%" src="logo-icon.gif"></a>
+                    </div>
+                </div>
+				<div class="col-md-4 col-sm-2 col-xs-12  ">
+                    <div class="navbar5-logo col-md-12 col-sm-12 col-xs-12 padding-top-20  padding-bottom-0 text-right">
+                        <a href="https://www.facebook.com/navarasangalCompany" target=”_blank” class="" style="padding:20px;"><img alt=""  style="width:10%" src="facebook.png"></a>
+					<a href="https://www.youtube.com/channel/UCgAjPgjfaQbzapZuSsN80Mg" target=”_blank” class="" style="padding:20px 0px 20px 0px;"><img alt=""  style="width:10%" src="youtube.png"></a>
+						<a href="https://www.instagram.com/navarasangal/" target=”_blank” class="" style="padding:20px;"><img alt=""  style="width:10%" src="instagram.png"></a>
+						
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <!--==========================================footer section=============================================-->
+    <div class="footer3 padding-top-70 maindv-content2" style="display:none !important;">
+        <div class="container">
+            <div class="row">
+               
+                <div class="col-md-12 col-sm-12 col-xs-12 text-center" >
+                    <p style="font-size: 20px;  color: #e7ecf1;" >social@navarasangal.com</p>
+                </div>
+               
+                <div class="clearfix"></div>
+                <div class="footer-copy padding-top-40 padding-bottom-40">
+                    <div class="text-center">
+                        <p>Copyright © 2020 Navarasangal.com, All Rights Reserved.</p>
+                    </div>
+
+                </div>
+                <div class="top-icon backtop">
+                    <a href="#topp"><i class="fa fa-chevron-up"></i></a>
+                </div>
+            </div>
+
+        </div>
+
+
+    </div>
+
+    <div id="loadspinner" class="row" style="background-color:#fff;display:none; color: #fff; border-color: #fff;">
+        <img src="logo_load.gif" />
+    </div>
+    <!-- Load JS here for greater good =============================-->
+
+    <script src="js/jquery.js"></script>
+
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/carousel/triger.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/migrate.js"></script>
+
+    <script src="js/owl.carousel.js"></script>
+
+    <script src="js/app4.js"></script>
+    <script src="js/jquery.easing.js"></script>
+    <script src="js/jquery.fancybox.pack.js"></script>
+    <script src="js/carousel/fancy.js"></script>
+    <script src="js/carousel/skills.js"></script>
+    <script type="text/javascript" src="js/jquery.hoverdir.js"></script>
+    <script src="js/carousel/contr.js"></script>
+    <script src="js/carousel/conn2.js"></script>
+    <script src="mail/jquery.ajaxchimp.min.js"></script>
+    <script src="mail/mailchimp.js"></script>
+
+    <script src="js/jquery.blockUI.js"></script>
+
+    <script type="text/javascript">
+    var hdr_banner = 0;
+
+    function toggleHeader() {
+        if (hdr_banner === 0) {
+            hdr_banner = 1;
+            $("#nav-version7").fadeOut();
+        }
+
+    }
+
+    function ShowMainContents() {
+        $.unblockUI();
+        $('.maindv-element').fadeIn(3000);
+    }
+
+    (function($) {
+        $(document).ready(function() {
+
+            $.blockUI({
+                message: $('#loadspinner'),
+                overlayCSS: {
+                    backgroundColor: '#fff',
+                    opacity: '1'
+                }
+            });
+            setTimeout(function() {
+                ShowMainContents();
+            }, 8000);
+            setTimeout(function() {
+                $('.maindv-content1').fadeIn(3000);
+            }, 9000);
+            setTimeout(function() {
+                $('.maindv-content2').fadeIn(3000);
+            }, 10000);
+
+
+            "use strict";
+            $(' #da-thumbs > li ').each(function() {
+                $(this).hoverdir();
+            });
+        });
+    })(jQuery);
+    </script>
+
+
+    <script type="text/javascript">
+    // unblock when ajax activity stops 
+    //$(document).ajaxStop($.unblockUI); 
+
+
+
+    /* 	$(document).ready(function() { 
+    		$.blockUI({ message: $('#loadspinner'), overlayCSS: { backgroundColor: '#fff', opacity:  '1'  } }); 
+    		setTimeout(function(){ $.unblockUI(); }, 5000);
+    	});  */
+    </script>
+
+
+
+
+</body>
+
+</html>
---
+++ demo/index17.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+     <title>Navarasangal.com</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    	 <!-- master css -->
+    <link href="css/master.css" rel="stylesheet">
+    <link href="css/color7.css" rel="stylesheet">
+   
+    <link href="css/hover.css" rel="stylesheet">
+   
+	<link rel="stylesheet" href="css/magnific-popup.css">
+	<script src="js/modernizr.custom.js"></script>
+    <link rel="shortcut icon" href="images/favicon.ico">
+	<link href='https://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800,900' rel='stylesheet' type='text/css'><!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body  >
+ 
+    <!--==========================================slider section=============================================-->
+	<div class="banner3 slider-version7 maindv-element" style="display:none !important;" id="topp">
+		<div class="container">
+			<div class="row  padding-top-30" >
+				
+				<div class="col-md-12 col-sm-12 col-xs-12 banner3-content padding-top-00" id="content-version7">	
+					
+					<div class="other-content text-center ">
+						<img src="navarasangal.gif" width="30%" />
+									
+					</div>
+					
+				</div>
+				
+				<div style="display:none;" class="top-address-bar" id="topbar-version7">
+					<div class="Center">
+						<img src="logo.gif"></div>
+				</div>
+			
+			</div>
+			
+		</div>
+	
+	</div>
+		 <!--==========================================TOP NAV=============================================-->
+	
+	
+	
+	<div class="service3-section padding-top-00 maindv-content1" style="display:none !important;" id="1">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 col-sm-8 col-xs-12 service3-main" id="service-version7">
+					
+<p class="text-white text-center padding-top-20" style="padding-left: 15%;padding-right: 15%;" >
+Navarsam is a significant concept in the Indian art and cultural history. It symbolizes the varied human emotions - anger, disgust, fear, sadness, wonder, joy, love, courage and peace. Aren't these the same emotions that we go through on a daily basis? Navarsangal.com is a humble attempt to trigger these varied emotions in you and share a platform to recognize the amazing ways in which you can portray those with the right level of originality, quality and intensity for entertainment. We are coming soon with more things to do here soon. Meanwhile enjoy some music and scroll down interesting content posted online by you, below.. 
+
+							
+				</p>
+
+					
+					<div class="clearfix"></div>
+					
+				</div>
+				
+			</div>
+			<div class="row padding-top-20">
+					<div class="content-head center">
+							
+					</div>
+			
+			</div>
+		
+		</div>
+	
+	</div>
+
+	<div class="service3-section padding-top-35 maindv-content2" style="background-color: #cccccc;display:none !important;" id="1">
+		<div class="container">
+			<div class="row">
+				
+				<div class="taggbox-container" style=" width:100%;height:100%;overflow: auto;background-color: #cccccc;"><script defer src="//web.taggbox.com/app/js/embed.min.js" type="text/javascript"></script><div class="taggbox-socialwall" data-wall-id="33674" view-url="https://taggbox.com/w/33674">  </div></div>
+				
+			</div>
+			<div class="row padding-top-20">
+					<div class="content-head center">
+							
+					</div>
+			
+			</div>
+		
+		</div>
+	
+	</div>
+		
+	 <!--==========================================subscribe form section=============================================-->
+
+	<div class="clearfix"></div>
+	
+	<div class="top-social3 padding-top-20 maindv-content2" style="display:none;" id="nav-version7">
+		
+			<div class="container top-nav clearfix maindv-content2-responsive ">
+				<div class="row">
+				<div class="col-md-2 col-sm-12 col-xs-12  text-center">
+							<div class="navbar5-logo col-md-12 col-sm-12 col-xs-12 text-center">	
+							<button class="button button5"
+							style="background-color: #fff;
+    border: 2px solid;
+    border-color:#f2f2f2;
+    padding: 20px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;border-radius: 50%;"
+							>50%</button>
+							<h4>Online</h4>
+							</div>
+					</div>
+					<div class="col-md-8 col-sm-12 col-xs-12  text-center">
+							<div class="navbar5-logo col-md-12 col-sm-12 col-xs-12 text-center">	
+								<a href="index.php" class=""><img alt="" style="width:30%"  src="logo.gif"></a>
+							</div>
+					</div>
+					<div class="col-md-2 col-sm-12 col-xs-12  text-center">
+							<div class="navbar5-logo col-md-12 col-sm-12 col-xs-12 text-center">	
+							<button class="button button5">Radio</button>
+							</div>
+					</div>
+						
+					
+			</div>
+		</div>
+    </div>
+	
+	    <!--==========================================footer section=============================================-->   
+		<div class="footer3 padding-top-70 maindv-content2" style="display:none !important;" >
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4 col-sm-4 col-xs-12 contact-add padding-left-0">	
+						<p>PO Box </p>
+					</div>
+					<div class="col-md-4 col-sm-4 col-xs-12 contact-email text-center" >	
+						<p>support</p>
+					</div>
+					<div class="col-md-4 col-sm-4 col-xs-12 contact-phone text-right padding-right-0">	
+						<p>+111233<br/>
+ </p>
+					</div>
+					<div class="clearfix"></div>
+					<div class="footer-copy padding-top-40 padding-bottom-40">
+						<div class="text-center">
+							<p>Copyright © 2020 Navarasangal.com, All Rights Reserved.</p>
+						</div>
+						
+					</div>
+					<div class="top-icon backtop">
+						<a href="#topp"><i class="fa fa-chevron-up"></i></a>
+					</div>
+				</div>
+			
+			</div>
+		
+		
+		</div>
+	
+		<div id="loadspinner" class="row" style="background-color:#fff;display:none; color: #fff; border-color: #fff;"> 
+			<img  src="navarasangal.gif" />
+		</div> 
+    <!-- Load JS here for greater good =============================-->
+	 
+	<script src="js/jquery.js"></script>
+	 
+	 <script src="js/bootstrap.min.js"></script>
+    <script src="js/carousel/triger.js"></script>
+	 <script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/migrate.js"></script>
+
+	<script src="js/owl.carousel.js"></script>
+	
+	<script src="js/app4.js"></script>
+	<script src="js/jquery.easing.js"></script>
+	<script src="js/jquery.fancybox.pack.js"></script>
+	<script src="js/carousel/fancy.js"></script>
+	 <script src="js/carousel/skills.js"></script>
+	 <script type="text/javascript" src="js/jquery.hoverdir.js"></script>	
+	 <script src="js/carousel/contr.js"></script>
+	<script src="js/carousel/conn2.js"></script>	
+	<script src="mail/jquery.ajaxchimp.min.js"></script>
+	<script src="mail/mailchimp.js"></script>
+	
+	<script src="js/jquery.blockUI.js"></script>
+
+		<script type="text/javascript">
+var hdr_banner = 0;
+		function toggleHeader()
+		{
+			if(hdr_banner  === 0)
+			{hdr_banner  = 1;
+				$("#nav-version7").fadeOut();   
+			}
+			
+		}
+
+		function ShowMainContents()
+		{
+			$.unblockUI(); 
+			$('.maindv-element').fadeIn(3000);
+		}
+
+			( function($) {
+				$(document).ready(function () {
+				
+				 	$.blockUI({ message: $('#loadspinner'), overlayCSS: { backgroundColor: '#fff', opacity:  '1'  } }); 
+					setTimeout(function(){ ShowMainContents();}, 8000); 
+					setTimeout(function(){ $('.maindv-content1').fadeIn(3000); }, 9000); 
+					setTimeout(function(){ $('.maindv-content2').fadeIn(3000);}, 10000); 
+					
+					
+					 "use strict";
+				$(' #da-thumbs > li ').each( function() { $(this).hoverdir(); } );
+			});
+			})(jQuery);
+		</script>
+	
+
+		<script type="text/javascript"> 
+ 
+			// unblock when ajax activity stops 
+			//$(document).ajaxStop($.unblockUI); 
+		 
+	
+		 
+		/* 	$(document).ready(function() { 
+				$.blockUI({ message: $('#loadspinner'), overlayCSS: { backgroundColor: '#fff', opacity:  '1'  } }); 
+				setTimeout(function(){ $.unblockUI(); }, 5000);
+			});  */
+		 
+		</script> 
+		 
+		
+		 
+		
+</body>
+</html>
